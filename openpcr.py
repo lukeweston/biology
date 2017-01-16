ca008bf7ef8d01dc74c97d0468bb12f6b88feb04
Luke Weston <reindeerflotilla@gmail.com>
2017-01-16 20:35:32 +1100

    initial commit

---
+++ openpcr.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+# OpenPyCR thanks to Cathal Garvey :)
+# I have fiddled with this and hacked it a little from the original. 
+# Free software under GNU AGPL v3 or later.
+
+import curses
+import time
+import sys
+import argparse
+from openpcrlib import OpenPCR
+import PCRCompiler
+
+class CursesDisplay():
+    def __init__(self, screen, dev):
+        self.scr = screen
+        self.dev = dev
+
+    def monitor(self):
+        self.scr.nodelay(1) # Make self.scr.getch nonblocking.
+        while True:
+            self.printStatusMsg()
+            time.sleep(1)
+            char = self.scr.getch()
+            if char != -1:
+                break
+
+    def writeln(self,string):
+        self.scr.erase() # Clear window
+        self.scr.addstr(0,0,string)
+        self.scr.refresh()
+
+    def printStatusMsg(self):
+        S = self.dev.readstatus()
+        try:
+            PrettyStatus = 'Welcome to the OpenPyCR Real-time Monitor.\n'
+            PrettyStatus += 'Current Program: {0}\n'.format(S['program'])
+            PrettyStatus += ' Step "{0}" of cycle {1}\n'.format(S['currentstep'],str(S['cycle']))
+            PrettyStatus += ' Currently: {0}\n'.format(S['job'])
+            PrettyStatus += ' Block: {0}C, Lid: {1}C\n'.format(str(S['blocktemp']),str(S['lidtemp']))
+            PrettyStatus += ' Remaining Time: {0}\n(Press any key to exit monitor mode)'.format(S['timeleft'])
+        except KeyError:
+            PrettyStatus = 'Run Finished.\n(Press any key to exit monitor mode)'
+        self.writeln(PrettyStatus)
+
+def CursesMonitor(dev):
+    stdscr = curses.initscr()
+    curses.cbreak()
+    Console = CursesDisplay(stdscr, dev)
+    Console.monitor()
+    curses.nocbreak()
+    curses.endwin()
+
+#======= Functions for Terminal Use Follow ========
+def status(device, args):
+    device.printstatus()
+
+def monitor(device, args):
+    CursesMonitor(device)
+
+def send(device, args):
+    with args.program_file as InF:
+        program = InF.read().strip()
+    program = PCRCompiler.parse_program(program)
+    device.sendprogram(program)
+
+def stop(device, args):
+    device.stop()
+    
+def log(device, args):
+    # args.interval and args.output_file
+    with args.output_file as LogF:
+        lastflush = time.time()
+        try:
+            while True:
+                LogLine = device.csvstatus(args.columns)
+                if device.active:
+                    LogF.write(LogLine+"\n")
+                else:
+                    break
+                if lastflush >= args.flush_interval:
+                    lastflush = time.time()
+                    LogF.flush()
+                time.sleep(args.interval)
+        except KeyboardInterrupt:
+            pass
+
+def pcrcompile(device, args):
+    with args.program_file as InF:
+        print(PCRCompiler.parse_program(InF.read()))
+
+P = argparse.ArgumentParser(
+                description = "OpenPyCR: A pure-python controller and monitor for the OpenPCR Thermal Cycler.",
+                epilog = "by Cathal Garvey, copyright 2013, released as Free Software under the GNU AGPL v3 or later.")
+P.add_argument("-m","--device-mountpoint",default="/media/OPENPCR",type=str,
+                    help="Path of the OpenPCR device. On Debian GNU/Linux (incl. Ubuntu) the default is usually correct.")
+Subs = P.add_subparsers(help="Subcommands: Try calling '-h' or '--help' after these to get more specific help, if available.")
+
+P_status = Subs.add_parser('status',help="Print a oen-time status message.")
+P_status.set_defaults(function = status)
+
+P_monitor = Subs.add_parser('monitor',help="Open a curses monitor for OpenPCR device.")
+P_monitor.set_defaults(function = monitor)
+
+P_send = Subs.add_parser('send',help="Send a string or file as a program to the OpenPCR device.")
+P_send.add_argument("-p","--program-file",type=argparse.FileType("r"),default=sys.stdin,
+                        help="Program to send. If not specified, reads from standard input.")
+P_send.set_defaults(function = send)
+
+P_stop = Subs.add_parser('stop',help='Send the stop signal to the OpenPCR to terminate current program.')
+P_stop.set_defaults(function = stop)
+
+P_log = Subs.add_parser('log',help='Print (or append to file) status information in csv format at set intervals.')
+P_log.set_defaults(function = log)
+P_log.add_argument("-i","--interval",type=int,default=5,help="Interval in seconds between log entries.")
+P_log.add_argument("-o","--output-file",type=argparse.FileType("a"),default=sys.stdout,
+                        help="File to append log output to. Default is stdout; print to terminal.")
+P_log.add_argument("--columns",nargs="+",type=str,default=['currenttime','elapsedsecs','cycle','blocktemp'],
+                        help="Columns to print to log. Options are: state job blocktemp lidtemp elapsedsecs secsleft currentstep cycle program nonce minsleft hoursleft timeleft currenttime")
+P_log.add_argument("--flush-interval",type=int,default=30,
+                        help="Interval by which to flush outut stream; may help prevent data loss on crash. If unsure, leave alone.")
+
+P_compile = Subs.add_parser('compile',help="Compile a program in OpenPyCR format to the lower-level OpenPCR format.")
+P_compile.set_defaults(function=pcrcompile)
+P_compile.add_argument("-p","--program-file",type=argparse.FileType("r"),default=sys.stdin,
+                        help="Program to send. If not specified, reads from standard input.")
+
+# Parse arguments, and pass arguments into the associated function for handling
+# according to appropriate subcommand.
+A = P.parse_args()
+Dev = OpenPCR(devicepath = A.device_mountpoint)
+if not hasattr(A,"function"):
+    P.print_usage()
+else:
+    A.function(Dev, A)
---
+++ openpcrlib.py
@@ -0,0 +1,157 @@
+import time
+import sys
+import os
+import collections
+
+if "linux" not in sys.platform:
+    print("OpenPyCR uses system calls that are only available on Linux platforms. Your platform -",
+            sys.platform,"- is probably incompatible, so reading OpenPCR status is probably impossible and this",
+            "program will probably crash. This is *not a bug* if you are using a platform other",
+            "than linux, and there is no plan to support non-free/libre platforms.", file=sys.stderr)
+if sys.version_info[:2] < (3,3):
+    # native posix_fadvise introduced in 3.3, can shim in with ctypes:
+    print("Your Python version is outdated and lacks the posix_fadvise system call in os.",
+          "Attempting to shim this in using ctypes..", file=sys.stderr)
+    import ctypes
+    try:
+        os.POSIX_FADV_NORMAL     = 0
+        os.POSIX_FADV_RANDOM     = 1
+        os.POSIX_FADV_SEQUENTIAL = 2
+        os.POSIX_FADV_WILLNEED   = 3
+        os.POSIX_FADV_DONTNEED   = 4
+        os.POSIX_FADV_NOREUSE    = 5
+        # The above will (or should?) always work, so do that first.
+        libc = ctypes.CDLL("libc.so.6")
+        os.posix_fadvise = libc.posix_fadvise
+        print("posix_fadvise shim successful, nothing to see here. Consider updating Python anyway.", file=sys.stderr)
+    except OSError:
+        print("Attempted to open libc.so.6 to import the posix_fadvise system call failed.",
+              "Reading from OpenPCR will not function correctly as disk/os level caching will interfere.",file=sys.stderr)
+        # Add an empty shim so it doesn't crash later.
+        if not hasattr(os, "posix_fadvise"): os.posix_fadvise = lambda w,x,y,a:None
+
+class OpenPCRError(Exception):
+    pass
+
+class OpenPCR:
+    def __init__(self,devicepath=''):
+        self.devicepath = devicepath or '/media/OPENPCR/'
+        self.active = False
+
+    @property
+    def ready(self):
+        return os.path.exists(self.devicepath) and os.path.exists(os.path.join(self.devicepath,"STATUS.TXT"))
+        
+    def sendprogram(self, program, status_callback=lambda x:None):
+        '''Sends a program to the OpenPCR and prints a verification if successful.
+        This can optionally send status messages (as a single string arg) to a callback.'''
+        if not self.ready:
+            raise OpenPCRError("Cannot send program as device is not ready.")
+
+#        NewNonce = self.readstatus()['nonce'] + 1 if CurrentNonce < 100 else 1 # Overflow; no need for excess digits.
+        NewNonce = self.readstatus()['nonce'] + 1
+	# OrderedDict preserves key order; may be critical for leading 's=ACGTC' signal.
+        dissectedprogram = collections.OrderedDict([x.split("=",1) for x in program.split("&")])
+        dissectedprogram['d'] = str(NewNonce)
+        self._sendprogram('&'.join(['='.join([x,y]) for x,y in dissectedprogram.items()]))
+
+        # Wait 2s to let OpenPCR recover from sending program, 
+        # then further 5s for program update.
+        status_callback("Waiting for two seconds for OpenPCR to resume responding.")
+        Status = time.sleep(2) # Now Status == None, not undefined.
+        started_waiting = time.time()
+        while time.time() - started_waiting < 5:
+            try:
+                Status = self.readstatus()
+                break
+            except ValueError:
+                status_callback("Still waiting for OpenPCR to respond..")
+                time.sleep(0.5)
+        else:
+            status_callback("Device failed to respond to status queries after sending updated program.")
+            raise OpenPCRError("Device failed to respond to status queries after sending updated program.")
+        # Printouts are nice and all but silent success and angry fail are more
+        # useful for building other applications on top of this.
+        if Status and Status['nonce'] == NewNonce:
+            status_callback("Program successfully sent.")
+        else:
+            status_callback("Program sent to device but device does not report receipt.")
+            raise OpenPCRError("Program sent to device but device does not report receipt.")
+
+    def _sendprogram(self,program):
+        with open(os.path.join(self.devicepath,'CONTROL.TXT'), mode='w') as Fout:
+            Fout.write(program)
+
+    def test(self):
+        # Expand here; this should test sending, sequential status reads, and stopping.
+        self.sendprogram('s=ACGTC&c=start&n=OpenPyCR Test Program&p=(20[10|45|TestStep1][20|25|TestStep2])')
+
+    def stop(self):
+        self.sendprogram('s=ACGTC&c=stop')
+
+    def ncc(self):
+        '''Low-level. Calls ncc binary for appropriate platform, returns raw output as string.
+        Behaves like "no-cache-cat" (ncc) but in pure-python. Only works on Unix, possibly Linux.
+        If this does not work correctly it is a silent failure; self-testing is essential
+        to ensure that non-caching reads are executed successfully. If not, fallback to
+        custom compiled C binaries would be necessary to get readouts.'''
+        if not self.ready:
+            raise OpenPCRError("Device not ready, cannot read status.")
+        filen = os.path.join(self.devicepath,'STATUS.TXT')
+        with open(filen,"rb") as InF:
+            os.posix_fadvise(InF.fileno(), 0, 0, os.POSIX_FADV_DONTNEED)
+            fc = InF.read()
+        # Return until first null character.
+        # Odd null/whitespace pattern is incompatible with unicode mode.
+        return fc.split(b"\0",1)[0].decode()
+
+    def readstatus(self):
+        'Calls ncc and translates output into a dictionary of values.'
+        statustxt = self.ncc()
+        status = dict([x.split("=") for x in statustxt.split("&")])
+        statusd = {'state': status.get('s','Unknown'),
+                   'job': status.get('t','Unknown'),
+                   'blocktemp': float(status.get('b',0)),
+                   'lidtemp': float(status.get('l',0)),
+                   'elapsedsecs': int(status.get('e',0)),
+                   'secsleft': int(status.get('r',0)),
+                   'currentstep': status.get('p','Unknown'),
+                   'cycle': int(status.get('c',0)),
+                   'program': status.get('n','Unknown'),
+                   'nonce': int(status.get('d',-1)),
+                   }
+        self.active = False if statusd['state'] in ['Complete','Inactive'] else True
+        if statusd['nonce'] == -1:
+            raise IOError("Received no program-identifier number from device - failure to communicate/reprogram?") 
+        # Now to clean up TIME ITSELF
+        statusd['minsleft'] = statusd['secsleft'] // 60
+        statusd['hoursleft'] = statusd['minsleft'] // 60
+        extramins = statusd['minsleft'] - (statusd['hoursleft'] * 60)
+        extrasecs = statusd['secsleft'] - (statusd['minsleft'] * 60)
+        statusd['timeleft'] = '{0}:{1}:{2}'.format(statusd['hoursleft'], extramins, extrasecs)
+        statusd['currenttime'] = time.strftime("%H:%M:%S",time.localtime())
+        return statusd
+
+    def csvstatus(self,keyorder = ['currenttime','elapsedsecs','cycle','blocktemp']):
+        '''Calls readstatus and formats output for logging to csv.
+
+        keyorder is a list of dictionary keys to use, in desired order, when
+        formatting output. The default provides the elapsed time in seconds,
+        current cycle number, and temperature of the block.'''
+        # Assemble keyworded formatting string from keyorder argument, then
+        # unpack the dictionary result of self.readstatus into it to get return.
+        return ', '.join(['{'+k+'}' for k in keyorder]).format(**self.readstatus())
+
+    def printstatus(self):
+        'Calls readstatus and prints useful information to stdout.'
+        S = self.readstatus()
+        if S['state'] == 'complete':  print("Program complete.")
+        elif S['state'] == 'stopped': print("System idle. No program running.")
+        elif S['state'] == "Unknown": print("System state unknown.")
+        else:
+            print(('Current Program: {program}\n'
+                   "Step '{currentstep}' of cycle {cycle}\n"
+                   "Currently: {job}\n"
+                   "Block: {blocktemp}°C, Lid: {lidtemp}°C\n"
+                   "Remaining Time: {timeleft}").format(**S))
+   
